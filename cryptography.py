cc0db4170ea1f90cd28fc81a2f3a21416e8438eb
Alexander Wu <alexanderwu68@yahoo.com>
2021-01-07 22:52:29 -0800

    Update cryptography.py to allow custom file and code-friendly key gen

--- cryptography.py
+++ cryptography.py
@@ -10,36 +10,48 @@ from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 from Crypto.Hash import SHA256
 import binascii
 
-pri_key_file_name = "examples/private_key.pem"
-pub_key_file_name = "examples/public_key.pem"
+example_pub_key_file_name = "examples/public_key.pem"
+example_pri_key_file_name = "examples/private_key.pem"
 
-def rsa_key_gen():
+def rsa_key_gen(file_names=None, code_friendly=False):
     """
     Returns an RSA key pair for signing in addition to printout.
     Saves them to the default file.
     """
+    print("Generating a pair of RSA keys...")
+
     key_pair = RSA.generate(bits=1024)
     print(f"Your public key is\n(n={hex(key_pair.n)}, e={hex(key_pair.e)})\n"
            "Publicly establish this key as belonging to you.\n")
     print(f"Your private key is\n(n={hex(key_pair.n)}, d={hex(key_pair.d)})\n"
            "Do not reveal this key (the d value) to anyone.\n")
 
-    with open(pri_key_file_name, 'wb+') as f:
-        f.write(key_pair.export_key('PEM'))
-        with open(pub_key_file_name, 'wb+') as f:
-            f.write(key_pair.publickey().export_key('PEM'))
-            print(f"Your public key has been saved to the file {pub_key_file_name}, "
-                  f"and you private key to {pri_key_file_name}. Do not share the private "
-                   "key file with anyone.\n")
+    if file_names:
+        pub_file_name, pri_file_name = file_names
+        with open(pub_file_name, 'wb+') as f1:
+            f1.write(key_pair.publickey().export_key('PEM'))
+            with open(pri_file_name, 'wb+') as f2:
+                f2.write(key_pair.export_key('PEM'))
+                print(f"Your public key has been saved to the file {pub_file_name}, "
+                      f"and you private key to {pri_file_name}. Do not share the private "
+                      "key file with anyone.\n")
+
+    if code_friendly:
+        print("Python-friendly ouput:\n")
+        print(f"n = {hex(key_pair.n)}")
+        print(f"e = {hex(key_pair.e)}")
+        print(f"d = {hex(key_pair.d)}")
+        print(f"public_key = Crypto.PublicKey.RSA.construct((n, e))")
+        print(f"private_key = Crypto.PublicKey.RSA.construct((n, e, d))\n")
     return key_pair
 
-def rsa_sign(message, key_pair=None):
+def rsa_sign(message, key_pair):
     """
     Returns the RSA signature in addition to printout.
-    If the key is not given, imports private key from default file.
+    If key_pair is a string, imports private key from this file.
     """
-    if not key_pair:
-        with open(pri_key_file_name, 'r') as f:
+    if isinstance(key_pair, str):
+        with open(key_pair, 'r') as f:
             key_pair = RSA.import_key(f.read())
     # Sign the message using the PKCS#1 v1.5 signature scheme (RSASP1).
     msg_hash = SHA256.new(message)
@@ -49,13 +61,13 @@ def rsa_sign(message, key_pair=None):
            "Share this signature along with your message to prove its authenticity.\n")
     return signature
 
-def rsa_verify(message, signature, public_key=None):
+def rsa_verify(message, signature, public_key):
     """
     Returns if the RSA signature is valid in addition to printout.
-    If the public key is not given, imports public key from default file.
+    If key_pair is a string, imports public key from this file.
     """
-    if not public_key:
-        with open(pub_key_file_name, 'r') as f:
+    if isinstance(key_pair, str):
+        with open(key_pair, 'r') as f:
             public_key = RSA.import_key(f.read())
     # Verify PKCS#1 v1.5 signature (RSAVP1).
     msg_hash = SHA256.new(message)
@@ -70,7 +82,7 @@ def rsa_verify(message, signature, public_key=None):
 
 def read_message_from_file(filename):
     """ Reads the file and returns the messaged encoded from ASCII to bytes. """
-    with open(filename,'r') as f:
+    with open(filename, 'r') as f:
         return bytes(f.read(), "ASCII")
     print(f"Failed to read file {filename}.")
 
@@ -85,11 +97,12 @@ if __name__ == "__main__":
     message = b"I'm glad I'm me, I'm glad I'm me, there's no one else I want to be."
 
     if GEN_NEW_KEY:
-        key_pair = rsa_key_gen()
+        key_pair = rsa_key_gen((example_pub_key_file_name, example_pri_key_file_name))
         public_key = key_pair.publickey()
         signature = rsa_sign(message, key_pair)
     else:
-        key_pair, public_key = None, None # If None, reads from file.
+        # If strings, reads from file.
+        public_key, key_pair = example_pub_key_file_name, example_pri_key_file_name
         signature = binascii.unhexlify(
             b"9450d44ba8b59c949c848a755dcb1683c1596d56e4efb2b7159bdc0853d4cd3a"
             b"14361bebb6204c1b3ecfd8981ab242055b4d846f1cd1e94d6a4277a0d455603c"
